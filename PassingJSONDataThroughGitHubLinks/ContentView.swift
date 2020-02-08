1677f453561306f888cc4591bc19f0fa0848d5fc
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-02-08 18:34:02 +0200

    init

--- PassingJSONDataThroughGitHubLinks/ContentView.swift
+++ PassingJSONDataThroughGitHubLinks/ContentView.swift
@@ -9,13 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var insta = instaData()
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                ForEach(insta.instas, id: \.id) { inst in
+                    VStack {
+                        Image(inst.image)
+                            .resizable()
+                            .frame(width: UIScreen.main.bounds.width - 34,
+                                   height: 200)
+                            .cornerRadius(12)
+                        
+                        Text(inst.names)
+                            .bold()
+                            .font(.subheadline)
+                            .foregroundColor(.primary)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Instagram: Decodable, Identifiable {
+    var id: Int
+    var names: String
+    var image: String
+    
+    enum CodinKey: String, CodingKey {
+        case id = "id"
+        case names = "names"
+        case image = "image"
+    }
+}
+
+public class instaData: ObservableObject {
+    @Published var instas = [Instagram]()
+    
+    init() {
+        loadData()
+    }
+    
+    func loadData() {
+        let url = URL(string: "link goes here")!
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                if let dt = data {
+                    let decode = try JSONDecoder().decode([Instagram].self, from: dt)
+                    
+                    DispatchQueue.main.async {
+                        self.instas = decode
+                    }
+                } else { print("Data not found") }
+            } catch { print(error.localizedDescription) }
+        }
     }
 }
